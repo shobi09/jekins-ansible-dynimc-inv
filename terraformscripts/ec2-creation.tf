df6c8f423bb9d211c5c67f7af8ea65d4462f514d
Shobanalakshmi R <116582069+shobi09@users.noreply.github.com>
2023-01-16 22:50:06 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,15 @@
 provider "aws"{
-   region  = "ap-southeast-1" 
+   region  = "ap-south-1" 
 }
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 1
-     ami = "ami-010d564ad44bd1912"
+     ami = "ami-0f9d9a251c1a44858"
      instance_type = "t2.micro"
-     security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
+     security_groups = ["launch-wizard-3"]
+     key_name        = "devops"
      tags = {
-        Name = "tomcatservers"
+        Name = "sonar"
      }
 }
 
